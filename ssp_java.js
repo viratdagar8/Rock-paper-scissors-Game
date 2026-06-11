cc893774ae1bd1142e5bf044a956c2914e58b826
viratdagar8 <dagarvirat8@gmail.com>
2026-06-11 12:20:10 +0530

    Add files via upload

---
+++ ssp_java.js
@@ -0,0 +1,59 @@
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice=()=>{
+    const options =["rock","paper","scissors"];
+    const randId= Math.floor(Math.random()*3);
+    return options[randId];
+};
+
+const drawGame=()=>{
+    msg.innerText="It's a draw! Play again.";
+    msg.style.background="#081b31";
+};
+const showWinner=(userWin ,userChoice,compChoice)=>{
+    if(userWin){
+        userScore++;
+        userScorePara.innerText=` ${userScore}`;
+        msg.innerText=`You win! ${userChoice} beats ${compChoice}`;
+        msg.style.color="green";
+    } else {
+        compScore++;
+        compScorePara.innerText=` ${compScore}`;
+        msg.innerText=`Computer wins! ${compChoice} beats ${userChoice}`;
+        msg.style.color="red";
+    }
+};
+
+
+const playGame=(userChoice)=>{
+    const compChoice = genCompChoice();
+
+    if (userChoice === compChoice){
+    drawGame();
+}else{
+    let userWin =true;
+    if(userChoice==="rock"){
+        userWin = compChoice==="paper"?false:true;
+    } else if(userChoice==="paper"){
+        userWin = compChoice === "scissors"?false:true; 
+    }else if(userChoice==="scissors"){
+        userWin = compChoice === "rock"?false:true;
+    }
+    showWinner(userWin,userChoice,compChoice);
+}
+
+     
+}
+choices.forEach((choice) => {
+    choice.addEventListener("click",() =>{
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
